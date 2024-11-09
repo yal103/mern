71f54b547f14241773ddf4804c2edf31258aefe6
yal103 <yal103@ucsd.edu>
2024-11-09 02:52:18 -0800

    create nav bar, dark/light mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import { Box } from "@chakra-ui/react";
-import { Routes, Route } from "react-router-dom";
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+
 function App() {
 
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
