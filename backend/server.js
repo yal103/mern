b43b7e1510752a13c5a1874d3e55bca86c02d9dd
yal103 <yal103@ucsd.edu>
2024-11-09 01:03:14 -0800

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,21 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.post("/products", async (req, res) => {
-    const product = req.body; // user will send this data
+app.use(express.json()); // allows us to accept JSON data in the body
 
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success:false, message: "Please provide all fields"});
-    }
+app.use("/api/products", productRoutes);
 
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-        console.error("Error in Create product:", error.message);
-        res.status(500).json({ success: false, message: "Server Error"});
-    }
-});
-
-// Postman desktop app
 
 console.log(process.env.MONGO_URI);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server started at http://localhost:5000');
+    console.log('Server started at http://localhost:' + PORT);
 });
